b1945d6bec89932918f7f7fcc4049970f14d7bc3
Ray Donnelly <mingw.android@gmail.com>
2017-09-22 12:02:49 -0700

    oops

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
-python setup.py install --single-version-externally-managed --record=record.txt
+python setup.py install
 :: Conflicts with jupyter_core which is a transitive dep.
 del /s /q %SP_DIR%\jupyter.py
