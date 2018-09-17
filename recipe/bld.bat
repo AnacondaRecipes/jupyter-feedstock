541cf8c1f333a5340a876eacedcb23066dbc8581
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-17 16:02:31 -0500

    build 6, remove pyc/__pycache on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,3 +3,5 @@ pip install --no-deps .
 :: Conflicts with jupyter_core which is a transitive dep.
 cd %SP_DIR%
 del jupyter.py
+if exist jupyter.pyc del /Q jupyter.pyc
+if exist __pycache__ del /Q __pycache__
