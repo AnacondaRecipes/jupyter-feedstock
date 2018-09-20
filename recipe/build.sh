22def54c335eed025e0ae79c93d51fa23bc66d3f
Jonathan Helmus <jjhelmus@gmail.com>
2018-09-20 10:25:05 -0500

    build 7, remove .pyc files on unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ pip install --no-deps .
 # Conflicts with jupyter_core which is a transitive dep.
 cd $SP_DIR
 rm jupyter.py
+rm -f jupyter.pyc
 if [[ -d __pycache__ ]]; then
    rm -rf __pycache__
 fi
