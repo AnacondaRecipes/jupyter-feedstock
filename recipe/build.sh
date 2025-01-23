7042cc6d00095aa33e31b153aea99164e370d1c9
Brian Wing <bwing@anaconda.com>
2025-01-23 10:07:16 -0500

    Updated to 1.1.1
    Co-authored-by: Charles Bousseau <16641587+cbouss@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,2 @@
 export PYTHONDONTWRITEBYTECODE=1
 pip install --no-deps --no-build-isolation .
-# Conflicts with jupyter_core which is a transitive dep.
-cd $SP_DIR
-rm jupyter.py
-rm -f jupyter.pyc
-if [[ -d __pycache__ ]]; then
-   rm -rf __pycache__
-fi
